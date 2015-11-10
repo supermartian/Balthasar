03e76dd67dd98c198958a9514a0b5ed62842cf97
Yuzhong Wen <wyz2014@vt.edu>
2015-11-09 20:03:19 -0500

    Identification of pthread_mutex_lock

--- PthreadsAnalysis.cpp
+++ PthreadsAnalysis.cpp
@@ -5,23 +5,52 @@
  * Distributed under terms of the MIT license.
  */
 
+#define DEBUG_TYPE "pthread_"
 
 #include "llvm/Pass.h"
-#include "llvm/Support/CallSite.h"
-#include "llvm/Instructions.h"
-#include "llvm/Support/InstVisitor.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/CallSite.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/ModuleUtils.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Debug.h"
 
 using namespace llvm;
 
+STATISTIC(NumPthreadMutexLock, "Number of pthread_mutex_lock");
+
 namespace {
-  struct WrapPthreads : public BasicBlockPass {
-      virtual bool runOnBasicBlock(BasicBlock &BB) {
-      }
-  };
+/*
+ * pthread_mutex_lock would be wrapped by __NR_syscall_det_start and __NR_syscall_det_end
+ *
+ */
+struct WrapPthreads : public FunctionPass {
+    static char ID; // Pass identification, replacement for typeid
+    WrapPthreads() : FunctionPass(ID) {}
 
-  struct InsertTicks : public BasicBlockPass {
-  };
+    bool runOnFunction(Function &F) {
+        for (auto &BB : F) {
+            for (auto &Inst : BB) {
+                if (isa<CallInst>(Inst)) {
+                    TransformPthreadMutexLock(CallSite(&Inst));
+                }
+            }
+        }
+        return true;
+    }
 
-  RegisterPass<WrapPthreads> X("wrap-pthreads", "Instrument pthreads functions with determinstic hints");
-  RegisterPass<InsertTicks> X("insert-ticks", "Instrument code with determinstic ticks");
+    void TransformPthreadMutexLock(CallSite c) {
+        Function *fun = c.getCalledFunction();
+        // Let's assume there's no indirect pthread_lock calls...
+        if (fun) {
+            if (fun->getName() == "pthread_mutex_lock") {
+                NumPthreadMutexLock++;
+            }
+        }
+    }
+};
+
+char WrapPthreads::ID = 0;
+static RegisterPass<WrapPthreads> X("wrap-pthreads", "Instrument pthreads functions with determinstic hints");
 }

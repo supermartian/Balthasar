23b94037c53f440f08b7728deedb5e70f54923fe
Yuzhong Wen <wyz2014@vt.edu>
2015-11-10 16:42:54 -0500

    Wrapping around pthread function calls

--- PthreadsAnalysis.cpp
+++ PthreadsAnalysis.cpp
@@ -19,12 +19,13 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Debug.h"
 
+#define DET_BB_THRESHOLD 5
 
 using namespace llvm;
 
 namespace {
 
-#define DET_BB_THRESHOLD 3
+FunctionType *ftype = FunctionType::get(Type::getInt32Ty(getGlobalContext()), true);
 /*
  * pthread_mutex_lock would be wrapped by __NR_syscall_det_start and __NR_syscall_det_end
  *
@@ -37,18 +38,32 @@ struct WrapPthreads : public FunctionPass {
         for (auto &BB : F) {
             for (auto &Inst : BB) {
                 if (isa<CallInst>(Inst)) {
-                    TransformPthreadMutexLock(CallSite(&Inst));
+                    TransformPthreadPrimitive(&Inst);
                 }
             }
         }
         return true;
     }
 
-    void TransformPthreadMutexLock(CallSite c) {
-        Function *fun = c.getCalledFunction();
+    void TransformPthreadPrimitive(Instruction *inst) {
+        Function *fun = CallSite(inst).getCalledFunction();
         // Let's assume there's no indirect pthread_lock calls...
         if (fun) {
-            if (fun->getName() == "pthread_mutex_lock") {
+            if ( fun->getName().find( "pthread_" ) == 0 && 
+                 fun->getName().find( "pthread_mutexattr_" ) == std::string::npos &&
+                 fun->getName().find( "pthread_mutex_init" ) == std::string::npos &&
+                 fun->getName().find( "pthread_mutex_destroy" ) == std::string::npos &&
+                 fun->getName().find( "pthread_attr_" ) == std::string::npos) {
+                Module *M = inst->getModule();
+                if (M == nullptr) {
+                    return;
+                }
+
+                // Wrap the function around with 319 and 320
+                IRBuilder<> IRB(inst);
+                Function *DetFunc = cast<Function>(M->getOrInsertFunction("syscall", ftype));
+                CallInst::Create(DetFunc, {IRB.getInt32(319)})->insertBefore(inst);
+                CallInst::Create(DetFunc, {IRB.getInt32(320)})->insertAfter(inst);
             }
         }
     }
@@ -58,6 +73,10 @@ struct WrapPthreads : public FunctionPass {
 char WrapPthreads::ID = 0;
 static RegisterPass<WrapPthreads> X("wrap-pthreads", "Instrument pthreads functions with determinstic hints");
 
+/*
+ * Most BBs will end with __NR_syscall_det_tick
+ *
+ */
 struct InsertDetTicks : public BasicBlockPass {
     static char ID; // Pass identification, replacement for typeid
     InsertDetTicks() : BasicBlockPass(ID) {}
@@ -67,20 +86,16 @@ struct InsertDetTicks : public BasicBlockPass {
             return false;
         }
 
-        if (BB.getTerminator() == nullptr) {
-            return false;
-        }
-
         Module *M = BB.getModule();
         if (M == nullptr) {
             return false;
         }
 
         IRBuilder<> IRB(&BB.back());
-        Function *DetTickFunc = cast<Function>(M->getOrInsertFunction("syscall", IRB.getInt32Ty(), IRB.getInt32Ty(), IRB.getInt64Ty(), nullptr));
-        //CallInst *DetTickInst = CallInst::Create(DetTickFunc, {IRB.getInt32(321), IRB.getInt64(BB.size())});
 
-        IRB.CreateCall(DetTickFunc, {IRB.getInt32(321), IRB.getInt64(BB.size())});
+        // Insert the tick count at the end of BB
+        Function *DetFunc = cast<Function>(M->getOrInsertFunction("syscall", ftype));
+        IRB.CreateCall(DetFunc, {IRB.getInt32(321), IRB.getInt64(BB.size())});
         errs() << "BB with inst count: " << BB.size() << "\n";
         return true;
     }
